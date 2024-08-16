4ddbec64a84a9b5c31bec827a87f640d158d9dc3
seangolden11 <seangolden11111@gmail.com>
2024-08-16 16:49:50 +0900

    .
    .

--- voyagersurvival/Assets/Script/Asteroid.cs
+++ voyagersurvival/Assets/Script/Asteroid.cs
@@ -9,12 +9,15 @@ public class Asteroid : MonoBehaviour
     bool isAlive;
     Vector2 dirvec;
     public float speed;
+    Vector2 spawnPos;
+    public float lifeTime;
 
     void Start()
     {
         rigid = GetComponent<Rigidbody>();
         dirvec = Random.insideUnitCircle.normalized;
         isAlive = true;
+        spawnPos = transform.position;
     }
 
     private void FixedUpdate()
@@ -28,6 +31,12 @@ public class Asteroid : MonoBehaviour
         nextvec.z = 0;
 
         rigid.MovePosition(rigid.position + nextvec);
+
+        if (Vector2.Distance(transform.position, spawnPos) > lifeTime)
+        {
+            isAlive = false;
+            this.gameObject.SetActive(false);
+        }
         
     }
 
@@ -41,5 +50,6 @@ public class Asteroid : MonoBehaviour
     private void OnEnable()
     {
         dirvec = Random.insideUnitCircle.normalized;
+        spawnPos = transform.position;
     }
 }
--- voyagersurvival/Assets/Script/Spawner.cs
+++ voyagersurvival/Assets/Script/Spawner.cs
@@ -7,10 +7,29 @@ public class Spawner : MonoBehaviour
 {
     PoolManager poolman;
 
+    float timer;
+
+
     void Start()
     {
         poolman = GameManger.Instance.pool;
     }
 
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer > 0.2f)
+        {
+            timer = 0;
+            Spawn();
+        }
+    }
+
+    void Spawn()
+    {
+        GameObject asteroid = GameManger.Instance.pool.Get(Random.Range(0, 2));
+        asteroid.transform.position = Vector3.zero;
+    }
 
 }
